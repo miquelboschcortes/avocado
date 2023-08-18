3fc3a2ebc9d861139db2097e5c393fa20c7143cb
Miquel Bosch <miquel.bosch.cortes@playsportsnetwork.com>
2023-08-18 17:33:55 +0200

    initial commit: resources

---
+++ avocado/avocado/avocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  avocadoApp.swift
+//  avocado
+//
+//  Created by Miguel Bosch Cortés on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct avocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
